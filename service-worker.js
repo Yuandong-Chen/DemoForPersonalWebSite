c1a023493078ea2b888003b0dc446ff0d0206197
Yuandong-Chen <YuandongChen@macair.local>
2019-01-30 04:55:23 -0500

    lab1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c826925b20b12fa9378b0e94424027b.js"
+  "/DemoForPersonalWebSite/precache-manifest.f4cc1afdda37877fed309f281afe5597.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/DemoForPersonalWebSite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
